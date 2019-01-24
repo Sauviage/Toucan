a977a38c5e8d0e2450d03b0ed35475d46238abcb
Layer-Etna <alary20@hotmail.fr>
2019-01-24 17:58:25 +0100

    Possibiliter d'update l'img des articles dans le portfolio

--- views/controller/update-portfolio.php
+++ views/controller/update-portfolio.php
@@ -14,18 +14,38 @@ $nom = "salut";
 
 if(!empty($_SESSION['username'])){
 
+
+    var_dump($_FILES);
     var_dump($_POST);
     require 'database.php';
 
-    $requete = $bdd->prepare('UPDATE projets SET icon = :icon, lien = :lien, titre = :titre, soustitre = :soustitre, adresse_img = :adresse_img WHERE id=:id');
-    $requete->execute(array(
-        'icon' => $_POST['icon'],
-        'lien' => $_POST['lien'],
-        'titre' => $_POST['titre'],
-        'soustitre' => $_POST['soustitre'],
-        'adresse_img' => $nom,
-        'id' => $_POST['id']
-    ));
+    if(!empty($_FILES)){
+
+        $nom = getcwd() . "\\..\\img\\" .$_FILES['adresse_img']['name'];
+
+        $resultat = move_uploaded_file($_FILES['adresse_img']['tmp_name'], $nom);
+
+        $requete = $bdd->prepare('UPDATE projets SET icon = :icon, lien = :lien, titre = :titre, soustitre = :soustitre, adresse_img = :adresse_img WHERE id=:id');
+        $requete->execute(array(
+            'icon' => $_POST['icon'],
+            'lien' => $_POST['lien'],
+            'titre' => $_POST['titre'],
+            'soustitre' => $_POST['soustitre'],
+            'adresse_img' => $_FILES['adresse_img']['name'],
+            'id' => $_POST['id']
+        ));
+    }else{
+        $requete = $bdd->prepare('UPDATE projets SET icon = :icon, lien = :lien, titre = :titre, soustitre = :soustitre WHERE id=:id');
+        $requete->execute(array(
+            'icon' => $_POST['icon'],
+            'lien' => $_POST['lien'],
+            'titre' => $_POST['titre'],
+            'soustitre' => $_POST['soustitre'],
+            'id' => $_POST['id']
+        ));
+    }
+
+
 
     echo "1";
 }
